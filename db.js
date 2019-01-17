43d77cc7070bf7977ec4aca5ee37d7c9400f357d
Vesna Munjiza <vesna@Vesnas-MacBook-Pro.local>
2019-01-17 12:54:20 +0100

    added 3 user functions

--- db.js
+++ db.js
@@ -2,10 +2,10 @@ const spicedPg = require("spiced-pg");
 const { dbUser, dbPass } = require("./secrets");
 const db = spicedPg(`postgres:${dbUser}:${dbPass}@localhost:5432/petition`);
 
-module.exports.addSignature = function(first, last, signature) {
+module.exports.addSignature = (first, last, signature, userId) => {
     return db.query(
-        `INSERT INTO signatures (first, last, signature) VALUES ($1, $2, $3)`,
-        [first, last, signature]
+        `INSERT INTO signatures (first, last, signature , user_id) VALUES ($1, $2, $3, $4) RETURNING id`,
+        [first, last, signature, userId]
     );
 };
 
@@ -22,4 +22,45 @@ module.exports.getSigners = function() {
 //         [first, last, signature]
 //     );
 // };
-//figure out how to take it from results and put it in the signatureId (in server)
+
+module.exports.getSig = function(sigId) {
+    return db.query(
+        `SELECT signature
+       FROM signatures WHERE id = $1`,
+        [sigId]
+    );
+};
+
+module.exports.registerUser = (first, last, email, hash) => {
+    return db.query(
+        `INSERT INTO users (first, last, email, password) VALUES ($1, $2, $3, $4) RETURNING id`,
+        [first, last, email, hash]
+    );
+};
+
+module.exports.getUserInfo = function(email) {
+    return db.query(
+        `SELECT *
+        FROM users
+        WHERE email = $1`,
+        [email]
+    );
+};
+
+module.exports.getUserPass = function(email) {
+    return db.query(
+        `SELECT password, id
+        FROM users
+        WHERE email = $1`,
+        [email]
+    );
+};
+
+module.exports.getUserId = function(email) {
+    return db.query(
+        `SELECT id
+        FROM users
+        WHERE email = $1`,
+        [email]
+    );
+};
