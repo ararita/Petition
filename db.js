f43f1d2fd9ab17687b7e1a4c5d05252f0a779f0c
Vesna Munjiza <vesna@Vesnas-MacBook-Pro.local>
2019-01-17 18:14:55 +0100

    added getSigners, part4

--- db.js
+++ db.js
@@ -2,18 +2,38 @@ const spicedPg = require("spiced-pg");
 const { dbUser, dbPass } = require("./secrets");
 const db = spicedPg(`postgres:${dbUser}:${dbPass}@localhost:5432/petition`);
 
-module.exports.addSignature = (first, last, signature, userId) => {
+module.exports.addSignature = (signature, userId) => {
     return db.query(
-        `INSERT INTO signatures (first, last, signature , user_id) VALUES ($1, $2, $3, $4) RETURNING id`,
-        [first, last, signature, userId]
+        `INSERT INTO signatures (signature , user_id) VALUES ($1, $2) RETURNING id`,
+        [signature, userId]
+    );
+};
+
+module.exports.addProfile = (age, city, homepage, userId) => {
+    return db.query(
+        `INSERT INTO user_profiles (age, city, url, user_id) VALUES ($1, $2, $3, $4)
+        RETURNING id`,
+        [
+            age ? Number(age) : null || null,
+            city || null,
+            homepage || null,
+            userId
+        ]
     );
 };
 
 module.exports.getSigners = function() {
-    return db.query(`SELECT first, last FROM signatures`).catch(err => {
-        console.log(err);
-    });
+    return db
+        .query(
+            `SELECT first, last FROM signatures
+        LEFT JOIN users
+        ON signatures.user_id = users.id`
+        )
+        .catch(err => {
+            console.log(err);
+        });
 };
+
 // module.exports.signPetition = function(first, last, signature) {
 //     return db.query(
 //         `INSERT INTO signatures (first, last, signature)
